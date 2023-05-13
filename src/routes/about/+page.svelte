4ef9d225f13aec7689b6f11ffec51da92f76f434
Jorge Corzo <jorgeach111@hotmail.com>
2023-05-13 23:04:06 +0930

    1 commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Acerca de</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Acerca de</h1>
+
+	<p>
+		Pronto... Nueva pagina web....
+	</p>
+</div>
